853c12e0e482ae459ab814ccc7acf794cf390177
CyedTechnologies <inam101@gmail.com>
2022-06-04 22:41:33 -0400

    added a new Method to CarsTest class

--- src/test/java/userInterface/CarsTest.java
+++ src/test/java/userInterface/CarsTest.java
@@ -1,12 +1,18 @@
 package userInterface;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class CarsTest {
+	@BeforeTest
+	public void DifferentModels()
+	{
+		System.out.println("These are the different Models of Cars");
+	}
 	@Test
 	public void Toyota ()
 	{
-		System.out.println("Corolla");
+		System.out.println("Corolla 2018");
 		System.out.println("RAV4");
 		System.out.println("Camry");
 		System.out.println("Prius");
@@ -16,10 +22,10 @@ public class CarsTest {
 	@Test
 	public void Honda ()
 	{
-		System.out.println("Civic");
+		System.out.println("Civic 2001");
 		System.out.println("Accord");
-		System.out.println("Civic");
-		System.out.println("Civic");
+		System.out.println("Civic 2002");
+		System.out.println("Civic 2003");
 	}
 	@Test
 	public void Tesla ()
